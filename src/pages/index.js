3c2b0ade571f068763d15820fcfdd2c51c658976
Nazar Bevzenko <nazarbevzenko@gmail.com>
2019-04-21 17:50:44 -0500

    Step 1 Gatsby tutorial.
    Added random image, larger, purple text and short paragraph.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{ color: `purple` }}>
+  <h1>Hello Gatsby!</h1>
+  <p> Gatsby code!</p>
+  <img src="https://source.unsplash.com/random/600x400" alt="https://source.unsplash.com/random/400x200" />
+  </div>
+)
