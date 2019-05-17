747dd5b2acb04183554e0b9fb65990917ace734e
Nazar Bevzenko <nazarbevzenko@gmail.com>
2019-05-16 21:09:01 -0500

    Photo Fill

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,11 @@ export default () => (
 <nav>
 <ul>
 <li><a href="localhost:8000/">Home</a></li>
-<li><a href="localhost:8000/nature">Nature</a></li>
-<li><a href="localhost:8000/urban">Urban</a></li>
-<li><a href="localhost:8000/animals">Animals</a></li>
-<li><a href="localhost:8000/skies">Skies</a></li>
-<li><a href="localhost:8000/credits">Credits</a></li>
+<li><a href="localhost:8000/">Nature</a></li>
+<li><a href="localhost:8000/">Urban</a></li>
+<li><a href="localhost:8000/">Animals</a></li>
+<li><a href="localhost:8000/">Skies</a></li>
+<li><a href="localhost:8000/">Credits</a></li>
 </ul></nav>
 </header>
 <label for="nav-toggle" class="nav-toggle-label">
