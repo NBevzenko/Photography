2432b4c98b78959afaf2ef6249a33d80c0cc53da
Nazar Bevzenko <nazarbevzenko@gmail.com>
2019-05-16 21:41:18 -0500

    Welcome text

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,18 +8,21 @@ export default () => (
      <input type="checkbox" id="nav-toggle" class="nav-toggle"/> 
 <nav>
 <ul>
-<li><a href="localhost:8000/">Home</a></li>
-<li><a href="localhost:8000/">Nature</a></li>
-<li><a href="localhost:8000/">Urban</a></li>
-<li><a href="localhost:8000/">Animals</a></li>
-<li><a href="localhost:8000/">Skies</a></li>
-<li><a href="localhost:8000/">Credits</a></li>
+<li><a href="https://source.unsplash.com/random">Home</a></li>
+<li><a href="#">Random Image</a></li>
+<li><a href="#">Remove Text</a></li>
+<li><a href="#">Show Text</a></li>
+<li><a href="#">About</a></li>
 </ul></nav>
 </header>
 <label for="nav-toggle" class="nav-toggle-label">
 </label>
 <div class="content">
-<h>TEMPORARY TEXT</h>
+<h> Welcome to my website! 
+This is an interactive photography website that I did for my tech finals project in 7th grade. 
+Press random image to recieve a random photo from the internet. Press hide text to hide this text. 
+And If you want to read this again, press show text! 
+If you want to know more about who I am, click about.</h>
 </div> 
 </div>
 )
